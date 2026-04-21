4c91aab5e0162182ef1b3b0521b7820f344e7c86
Prajwal Kedari <65669641+prajwalkedari@users.noreply.github.com>
2026-04-21 23:50:49 +0530

    Add files via upload

---
+++ custom_file_analyze.py
@@ -0,0 +1,10 @@
+import pickle
+import static.feature_extract
+
+with open(r'./static/models/smartshield-V1.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+def custom_analyze_file(file_path):
+    dataV = static.feature_extract.extract_features(file_path)
+    print("Extracted features:", dataV)
+    return (model.predict(dataV))
